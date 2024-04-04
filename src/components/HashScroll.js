59a8f9ed0fb2090d18b3dba60e543707cf67d868
Gina Häußge <gina@octoprint.org>
2024-04-04 09:20:21 +0200

    Only handle initial hash

--- src/components/HashScroll.js
+++ src/components/HashScroll.js
@@ -5,16 +5,9 @@ import {useLocation} from "react-use";
 export default function HashScroll(props) {
     const {hash} = useLocation();
     const scrolledRef = React.useRef(false);
-    const hashRef = React.useRef(hash);
 
     React.useEffect(() => {
         if (!hash) return;
-
-        if (hashRef.current !== hash) {
-            hashRef.current = hash;
-            scrolledRef.current = false;
-        }
-
         if (scrolledRef.current) return;
 
         const id = hash.replace("#", "");
